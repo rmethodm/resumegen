501182dc55a378a033a627fea7f8ed545147c107
rmethodm <rmethodm@outlook.com>
2026-06-02 10:57:32 -0400

    fix: correct admin usage route name (remove double admin prefix)

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::get('/r/{token}/pdf', [PublicResumeController::class, 'downloadPdf'])->na
 Route::post('/r/{token}/questions', [PublicResumeController::class, 'storeQuestion'])->middleware('throttle:5,1')->name('public.question');
 
 Route::middleware(['auth', 'master_admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/usage', [AdminUsageController::class, 'index'])->name('admin.usage');
+    Route::get('/usage', [AdminUsageController::class, 'index'])->name('usage');
     Route::get('/users', [AdminUserController::class, 'index'])->name('users.index');
     Route::patch('/users/{user}/toggle-pro', [AdminUserController::class, 'togglePro'])->name('users.toggle-pro');
     Route::delete('/users/{user}', [AdminUserController::class, 'destroy'])->name('users.destroy');
